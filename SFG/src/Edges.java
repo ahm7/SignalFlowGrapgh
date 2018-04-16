17d689bc1fdb7283969362fb32314d499f82f5f3
ahm7 <elpopahmed462@gmail.com>
2018-04-16 14:37:10 +0300

    the project

---
+++ SFG/src/Edges.java
@@ -0,0 +1,25 @@
+
+public class Edges {
+	
+	 private Node firstNode ;
+	 private Node secondNode;
+	 private String  value ; 
+	 public Edges(Node first ,Node second, String value){
+		 this.firstNode = first;
+		 this.secondNode=second;
+		 this.value = value;
+	 }
+
+	 public Node getFirstNode(){
+		 return this.firstNode;
+	 }
+	 public Node getSecondNode(){
+		 return this.secondNode;
+	 }
+	 public  String getValue(){
+		 return this.value;
+	 }
+	 
+	 
+	
+}
