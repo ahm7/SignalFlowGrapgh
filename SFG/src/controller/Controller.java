e416f055ff19914d807422b8b73d1e3a1e0fa262
ritasamir <ritasamir2587@gmail.com>
2018-04-19 02:27:07 -0700

    Update Controller.java

--- SFG/src/controller/Controller.java
+++ SFG/src/controller/Controller.java
@@ -1,5 +1,6 @@
 package controller;
 
+import gui.DrawingEngine;
 import gui.Frame;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -12,81 +13,118 @@ import model.Edge;
 import model.Node;
 
 public class Controller {
-	int nodeIndex=0;
-	static ArrayList<Node> nodesList;	///////////////////3'areha
-	static ArrayList<Edge> edgesList;///////////////////3'areha
+	int nodeIndex = 0;
+	static ArrayList<Node> nodesList; // /////////////////3'areha
+	static ArrayList<Edge> edgesList;// /////////////////3'areha
 	static Frame frame;
+	DrawingEngine drawingEngine;
 	static String shape;
-	Node first,second;
+	Node first, second;
+	String gain;
 
 	public Controller(Frame frame) {
-	this.frame=frame;
-	nodesList = new ArrayList<Node>();
-	edgesList = new ArrayList<Edge>();
+		this.frame = frame;
+		drawingEngine = new DrawingEngine();
+		nodesList = new ArrayList<Node>();
+		edgesList = new ArrayList<Edge>();
 	}
-	
+
 	public static void actionOnButtons(ActionEvent e) {
 
 		if (e.getSource() == frame.nodeBtn) {
-			shape="node";
+			shape = "node";
+			frame.selected("node");
 
-		}
-		else if (e.getSource() == frame.edgeBtn){
-			shape="edge";
-		}else if(e.getSource()==frame.calculateBtn){
+		} else if (e.getSource() == frame.edgeBtn) {
+			shape = "edge";
+			frame.selected("edge");
+		} else if (e.getSource() == frame.calculateBtn) {
 			print();
 		}
 
 	}
 
 	private static void print() {
-		for(Edge edge:edgesList)
-			System.out.println(edge.getFirstNode().getId()+" "+edge.getValue()+" "+edge.getSecondNode().getId());
+		for (Edge edge : edgesList)
+			System.out.println(edge.getFirstNode().getId() + " "
+					+ edge.getValue() + " " + edge.getSecondNode().getId());
 		System.out.println();
-		for(Node node:nodesList)
-		    System.out.println(node.getId());
+		for (Node node : nodesList)
+			System.out.println(node.getId());
 	}
 
 	public void actionOnSelect(MouseEvent e) {
-		if(shape.equals("node")){
+		if (shape.equals("node")) {
 			Point point = e.getPoint();
 			Node node = new Node(nodeIndex, point.x, point.y);
 			nodesList.add(node);
 			nodeIndex++;
-			node.draw(frame.drawArea.getG2());
-			frame.drawArea.repaint();
-		}else if(shape.equals("edge")){
+			drawingEngine.refresh(nodesList, edgesList, frame.drawArea);
+
+		} else if (shape.equals("edge")) {
 			Point point = e.getPoint();
-			Node node =isInsideNode(point.x, point.y); 
-			if(node!=null){
-				if(first==null){
-					first=node;
-				}else if(second==null){
-					second=node;
-					String gain = JOptionPane.showInputDialog("Enter the gain of your edge!");//draw edge
-					Edge edge = new Edge(first, second,gain);
-					edgesList.add(edge);
-					edge.draw(frame.drawArea.getG2());
-					frame.drawArea.repaint();
-					first=null;
-					second=null;
+			Node node = isInsideNode(point.x, point.y);
+			if (node != null) {
+				if (first == null) {
+					first = node;
+				} else if (second == null) {
+					second = node;
+					if(!checkInvalidEdge()){
+						gain = JOptionPane
+								.showInputDialog("Enter the gain of your edge!");// draw
+																					// edge
+					checkDrawEdge(Integer.toString(first.getId())
+							+ Integer.toString(second.getId()));
+					}else{
+						System.out.println("not valid Edge");
+					}
+					first = null;
+					second = null;
 				}
-			}else {
-				//show message not involving node 
-				System.out.println("not involving node"+point);
+			} else {
+				// show message not involving node
+				System.out.println("not involving node" + point);
 			}
-			
+
 		}
-		
+
 	}
-	 private Node isInsideNode(int x, int y) {
-	        for (Node node : nodesList) {
-	           if(x>node.getPositionX()&&x<node.getPositionX()+node.getWidth()){
-	        	   if(y>node.getPositionY()&&y<node.getPositionY()+node.getHeight()){
-	        		   return node;
-	        	   }
-	           }
-	        }
-	        return null;
-	    }
+
+	private boolean checkInvalidEdge() {
+		if(first.getId()==second.getId()){
+			return true;
+		}else{
+		return false;
+		}
+	}
+
+	private void checkDrawEdge(String string) {//check not repeated node if so make them one edge add their gains
+		boolean found = false;
+		for (Edge edge : edgesList) {
+			if (edge.getId().equals(string)) {
+				edge.setValue(gain + "+" + edge.getValue());
+				found = true;
+				drawingEngine.refresh(nodesList, edgesList, frame.drawArea);
+				break;
+			}
+		}
+		if (!found) {
+			Edge edge = new Edge(first, second, gain);
+			edgesList.add(edge);
+			drawingEngine.refresh(nodesList, edgesList, frame.drawArea);
+		}
+	}
+
+	private Node isInsideNode(int x, int y) {
+		for (Node node : nodesList) {
+			if (x > node.getPositionX()
+					&& x < node.getPositionX() + node.getWidth()) {
+				if (y > node.getPositionY()
+						&& y < node.getPositionY() + node.getHeight()) {
+					return node;
+				}
+			}
+		}
+		return null;
+	}
 }
